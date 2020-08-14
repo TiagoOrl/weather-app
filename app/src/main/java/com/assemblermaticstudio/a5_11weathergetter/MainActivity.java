1196c168d55b8061fdb6ef363b7f939c6019e845
Tiago Orlando <tiagoorlando2010@hotmail.com>
2020-08-13 22:27:19 -0300

    added the sample json files for comparison when it returns a weird position, i think the problem is with the gps coord

--- app/src/main/java/com/assemblermaticstudio/a5_11weathergetter/MainActivity.java
+++ app/src/main/java/com/assemblermaticstudio/a5_11weathergetter/MainActivity.java
@@ -16,6 +16,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
